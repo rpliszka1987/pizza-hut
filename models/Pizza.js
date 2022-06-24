e6152ad7cc7d93eb0be52ca9b5b4e6ea3b170160
Robert Pliszka <robplisz@gmail.com>
2022-06-24 15:58:58 -0400

    Created Comment model and added models to Pizza

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,46 @@
 const { Schema, model } = require("mongoose");
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    //   Adding comments model.
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
+  {
+    // Add virtuals to the model
+    toJSON: {
+      virtuals: true,
+    },
+    // Dont need id
+    id: false,
+  }
+);
+// get total count of comments and repliees on retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
