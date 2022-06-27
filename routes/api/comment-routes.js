1423c69c7ade38c2adeccd3d4014f6754867a323
Robert Pliszka <robplisz@gmail.com>
2022-06-27 11:10:40 -0400

    added reply to the routes and tested

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,12 +2,17 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
 // add comment api/:pizzaId
 router.route("/:pizzaId").post(addComment);
 
 // remove comment api/:pizzaId/:commentId
-router.route("/:pizzaId/:commentId").delete(removeComment);
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+
+// remove a comment reply
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
