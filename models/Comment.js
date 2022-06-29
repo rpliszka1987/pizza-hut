7e5bcab75ac0f1051a941076446ed545bd4dcf0b
Robert Pliszka <robplisz@gmail.com>
2022-06-28 21:28:23 -0400

    Added Validators to Comment and Reply models

--- models/Comment.js
+++ models/Comment.js
@@ -10,9 +10,13 @@ const ReplySchema = new Schema(
     },
     replyBody: {
       type: String,
+      required: true,
+      trim: true,
     },
     writtenBy: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdAt: {
       type: Date,
@@ -34,6 +38,8 @@ const CommentSchema = new Schema(
     },
     commentBody: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdAt: {
       type: Date,
